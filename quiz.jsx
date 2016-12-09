61ae7ee97fce9a555803f380bc9ed6c2da432881
Christopher <Button@ChristophersAir.att.net>
2016-12-08 17:25:31 -0800

    reworking the flipping

--- quiz.jsx
+++ quiz.jsx
@@ -12,10 +12,15 @@ export default class QuizStudy extends React.Component {
       this.state = {
         characterIdx: 0,
         card1: "card1",
-        card2: "card2"
+        card2: "card2",
+        card3: "card3",
+        card4: "card4"
       };
       this.toggleFlip1 = this.toggleFlip1.bind(this);
       this.toggleFlip2 = this.toggleFlip2.bind(this);
+      this.toggleFlip3 = this.toggleFlip3.bind(this);
+      this.toggleFlip4 = this.toggleFlip4.bind(this);
+
 
     }
 
@@ -41,6 +46,15 @@ export default class QuizStudy extends React.Component {
       this.setState({card2: "card2 flipped"});
     }
 
+    toggleFlip3() {
+      this.setState({card3: "card3 flipped"});
+    }
+
+    toggleFlip4() {
+      this.setState({card4: "card4 flipped"});
+    }
+
+
     render() {
       console.log(this.state.props);
       return(
@@ -54,24 +68,34 @@ export default class QuizStudy extends React.Component {
                src={this.state.props[this.state.characterIdx].jChar} />
           </div>
 
-         <div className="cardContainer">
+
+
+         <div className="cardContainerTop">
+
           <div className={this.state.card1} onClick={this.toggleFlip1}>
             <div className="card1 front">{this.state.props[this.state.characterIdx].eChar}</div>
             <div className="card1 back">O</div>
           </div>
+        </div>
+
           <div className={this.state.card2} onClick={this.toggleFlip2}>
             <div className="card2 front">{this.state.props[this.state.characterIdx].eChar}</div>
             <div className="card2 back">O</div>
           </div>
+
+        <div className="cardContainerBottom">
           <div className={this.state.card3} onClick={this.toggleFlip3}>
             <div className="card3 front">{this.state.props[this.state.characterIdx].eChar}</div>
             <div className="card3 back">O</div>
           </div>
+
           <div className={this.state.card4} onClick={this.toggleFlip4}>
             <div className="card4 front">{this.state.props[this.state.characterIdx].eChar}</div>
             <div className="card4 back">O</div>
           </div>
         </div>
+
+
         </div>
 
       );
